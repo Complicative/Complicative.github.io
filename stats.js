13942aacbb821999ccac103878cc1706d7cd2240
Complicative <mark.kleymann@outlook.com>
2022-11-14 21:05:50 +0100

    New Charts Library

--- stats.js
+++ stats.js
@@ -5,10 +5,12 @@ const myChart2 = document.getElementById('myChart2');
 const myChart3 = document.getElementById('myChart3');
 
 document.addEventListener("DOMContentLoaded", async () => {
-    //main(19);
+    main(3);
+    //console.log(getDate(getDay() - 1));
+
 });
 
-function test() {
+function goBtn() {
     main(input.value);
 }
 
@@ -24,9 +26,67 @@ async function main(days) {
 
     var xValues = [];
     for (let i = 0; i <= days; i++) {
+        xValues[i] = getDate(getDay() - days + i);
+    }
+
+    const chart = Highcharts.chart('container', {
+        chart: {
+            type: 'column'
+        },
+        title: {
+            text: 'PokeString Statistics'
+        },
+        xAxis: {
+            categories: xValues
+        },
+        yAxis: {
+            title: {
+                text: 'Values'
+            }
+        },
+        series: [{
+            name: 'Page Visits',
+            data: visits
+        }, {
+            name: 'Go Button Presses',
+            data: go
+        }, {
+            name: 'Copy Button Presses',
+            data: copy
+        }],
+        plotOptions: {
+            series: {
+                dataLabels: {
+                    enabled: true
+                },
+                stacking: "normal"
+            },
+            column: {
+                pointPadding: 0,
+                borderWidth: 0,
+                groupPadding: 0.05,
+                shadow: false
+
+            }
+        },
+        colors: ['#586F7C', '#EB5E28', '#AFFC41']
+    });
+}
+
+async function mainOld(days) {
+    let visits = [];
+    let go = [];
+    let copy = [];
+    for (let i = 0; i <= days; i++) {
+        visits[i] = await getVisits(getDay() - (days - i));
+        go[i] = await getGo(getDay() - (days - i));
+        copy[i] = await getCopy(getDay() - (days - i));
+    }
+
+    var xValues = [];
+    for (let i = 0; i <= days; i++) {
         xValues[i] = days - i;
     }
-    var yValues = visits;
 
     new Chart("myChart1", {
         type: "bar",
@@ -40,12 +100,10 @@ async function main(days) {
                 borderColor: "rgba(0,0,0)",
                 borderWidth: 1,
                 barPercentage: 0.99,
-                data: yValues
+                data: visits
             }]
         }
     });
-
-    yValues = go;
 
     new Chart("myChart2", {
         type: "bar",
@@ -59,12 +117,10 @@ async function main(days) {
                 borderColor: "rgba(0,0,0)",
                 borderWidth: 1,
                 barPercentage: 0.99,
-                data: yValues
+                data: go
             }]
         }
     });
-
-    yValues = copy;
 
     new Chart("myChart3", {
         type: "bar",
@@ -78,11 +134,22 @@ async function main(days) {
                 borderColor: "rgba(0,0,0)",
                 borderWidth: 1,
                 barPercentage: 0.99,
-                data: yValues
+                data: copy
             }]
         }
     });
 }
+
+input.addEventListener("keypress", function (event) {
+    console.log("test");
+    // If the user presses the "Enter" key on the keyboard
+    if (event.key === "Enter") {
+        // Cancel the default action, if needed
+        event.preventDefault();
+        // Trigger the button element with a click
+        document.getElementById("goBtn").click();
+    }
+});
 
 
 
@@ -90,6 +157,11 @@ function getDay() {
     const currentDay = Math.floor((Date.now() / 1000) / 86400);
     return currentDay;
 };
+
+function getDate(day) {
+    const newDay = new Date(day * 1000 * 86400);
+    return (newDay.getDate() + "." + (newDay.getMonth() + 1));
+}
 
 async function getVisits(day) {
     const response = await fetch("https://api.countapi.xyz/get/complicative.github.io/visits" + day)
